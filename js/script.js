b886ff1373921a8bd24db359c0e3eb694933b250
Kate Ray <email@kateray.net>
2011-08-01 15:53:35 -0400

    create new

--- js/script.js
+++ js/script.js
@@ -10,6 +10,14 @@ $(function() {
 	$('#create_pad').click(function(){
 		$('#new_pad_form').show();
 	})
+	$('#new_pad').submit(function(e){
+		e.preventDefault();
+		var title = $('#pad_title').val();
+		var pad = [{title: title}];	
+		$.tmpl(padTitleTemplate, pad).prependTo("#fileList");
+		$('#pad_title').val('');
+		$('#new_pad_form').hide();
+	})
 })
 
 
