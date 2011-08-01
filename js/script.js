42b0b922b9eb30d51e51100b8ed69f272be3fe6b
Kate Ray <email@kateray.net>
2011-08-01 15:13:54 -0400

    added jquery template

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 /* Author: 
 
 */
-
+$(function() {
+	var pads = [{title : "HTML5 Hackathon"},
+							{title : "Another Group"}
+							];
+	$.tmpl(padTitleTemplate, pads).appendTo("#sidebar");
+	$('#create_pad').click(function(){
+		$('#new_pad_form').show();
+	})
+	$('.pad_title').live('click', function(){
+		// load text and image
+	})
+})
 
 
 
