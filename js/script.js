794108a9e5a0cb943da13f04952222f7811e91a8
Ryan Giglio <ryangiglio@gmail.com>
2011-08-01 17:09:42 -0400

    Insert 4 spaces on tab

--- js/script.js
+++ js/script.js
@@ -1,6 +1,40 @@
 /* Author: 
 
 */
+function pasteHtmlAtCaret(html) {
+    var sel, range;
+    if (window.getSelection) {
+        // IE9 and non-IE
+        sel = window.getSelection();
+        if (sel.getRangeAt && sel.rangeCount) {
+            range = sel.getRangeAt(0);
+            range.deleteContents();
+
+            // Range.createContextualFragment() would be useful here but is
+            // non-standard and not supported in all browsers (IE9, for one)
+            var el = document.createElement("div");
+            el.innerHTML = html;
+            var frag = document.createDocumentFragment(), node, lastNode;
+            while ( (node = el.firstChild) ) {
+                lastNode = frag.appendChild(node);
+            }
+            range.insertNode(frag);
+
+            // Preserve the selection
+            if (lastNode) {
+                range = range.cloneRange();
+                range.setStartAfter(lastNode);
+                range.collapse(true);
+                sel.removeAllRanges();
+                sel.addRange(range);
+            }
+        }
+    } else if (document.selection && document.selection.type != "Control") {
+        // IE < 9
+        document.selection.createRange().pasteHTML(html);
+    }
+}
+
 $(function() {
 	$('#writeToggle').click(function(){
 		if ($(this).hasClass('activeMode')){
@@ -42,26 +76,10 @@ $(function() {
 			e.preventDefault();
 		}
 	})
+    $('#padText').keydown(function(e) {
+        if ( e.keyCode == 9) {
+            pasteHtmlAtCaret('&nbsp;&nbsp;&nbsp;&nbsp;');
+            e.preventDefault();
+        };
+    });
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
